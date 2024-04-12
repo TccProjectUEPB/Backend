0be2e7d5336e1ecd295d06f87542ce8bbf1551c6
gstv.rdrgs <gstvramone1@gmail.com>
2024-04-11 23:58:17 -0300

    feat: adding the status check for professor

--- src/infrastructure/database/schemas/professor.py
+++ src/infrastructure/database/schemas/professor.py
@@ -13,6 +13,7 @@ class Professor(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String(60))
     email = Column(String, unique=True)
+    status = Column(String(20))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
